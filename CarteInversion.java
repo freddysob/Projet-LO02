24686367bcda7f0bd2983ae64be18edc636d4b79
freddysob <32365266+freddysob@users.noreply.github.com>
2017-11-18 19:01:14 +0100

    Delete CarteInversion.java

--- CarteInversion.java
+++
@@ -1,8 +0,0 @@
-import com.modeliosoft.modelio.javadesigner.annotations.objid;
-
-@objid ("b7dfa75d-2d65-47dd-a8c8-b1a896bd0d85")
-public interface CarteInversion {
-    @objid ("109f50f1-ed96-43e4-8c14-2c327e1d730d")
-    void InverserSensJeu();
-
-}
