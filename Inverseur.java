d4b5f1d4ecae87876d9d0b0825dd0f57de18527c
freddysob <32365266+freddysob@users.noreply.github.com>
2017-11-20 19:11:27 +0100

    Update Inverseur.java

--- Inverseur.java
+++ Inverseur.java
@@ -1,8 +1,10 @@
-import com.modeliosoft.modelio.javadesigner.annotations.objid;
-
-@objid ("06625f01-a524-49a1-bdf9-2d0352967772")
 public class Inverseur extends Carte {
 	
+	public Inverseur(NumeroCarte num, TypCarte typ) {
+		super(num, typ, 54);
+		// TODO Auto-generated constructor stub
+	}
+
 	public void appliquerPouvoir() {
 	   
 	 }
