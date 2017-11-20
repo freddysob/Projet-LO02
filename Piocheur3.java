3fadac173997885248070fc0d93be7cb4124bfe6
freddysob <32365266+freddysob@users.noreply.github.com>
2017-11-20 19:14:07 +0100

    Update Piocheur3.java

--- Piocheur3.java
+++ Piocheur3.java
@@ -1,10 +1,12 @@
-import com.modeliosoft.modelio.javadesigner.annotations.objid;
-
-@objid ("6d52c023-be9e-40e8-8823-07a07dec11f8")
 public class Piocheur3 extends Carte {
 	
-	public void appliquerPouvoir() {
-		   
+	public Piocheur3(NumeroCarte num, TypCarte typ) {
+		super(num, typ, 15);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void appliquerPouvoir(Joueur joueur, Pioche pioche) {
+		pioche.distribuerCarte(3, joueur);   
 	 }
 	
 	public static void main(String[] args ) {
