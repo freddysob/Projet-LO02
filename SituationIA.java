3e81595cbe73150ba9fae973e878292e1d44ed53
freddysob <32365266+freddysob@users.noreply.github.com>
2017-11-20 19:15:24 +0100

    Update SituationIA.java

--- SituationIA.java
+++ SituationIA.java
@@ -1,8 +1,5 @@
-import com.modeliosoft.modelio.javadesigner.annotations.objid;
-
-@objid ("3f141fc2-2d20-4e7b-9a5b-67bd4847a3d3")
 public enum SituationIA {
     Normale,
-    1JoueurAvance,
-    2JoueursAvances;
+    _1JoueurAvance,
+    _2JoueursAvances;
 }
