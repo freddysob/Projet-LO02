e788433887c04e0713434198dc93bc94f7984565
freddysob <32365266+freddysob@users.noreply.github.com>
2017-11-20 19:29:24 +0100

    Update and rename typVariante.java to TypVariante.java

---
+++ TypVariante.java
@@ -0,0 +1,14 @@
+
+public enum TypVariante {
+    Minimale,
+    Monclar,
+    Variante_1,
+    Carte_et_Maou,
+    Des_Ulis,
+    Variante_4,
+    Variante_5,
+    Variante_6,
+    Variante_Courte_Amicale,
+    Man_et_resta;
+}
+
