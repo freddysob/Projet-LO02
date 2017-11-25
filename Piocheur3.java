8a9e6e3f536ca0d6b1b09f2121bf291ad897eb52
freddysob <32365266+freddysob@users.noreply.github.com>
2017-11-25 18:31:07 +0100

    Update Piocheur3.java

--- Piocheur3.java
+++ Piocheur3.java
@@ -1,12 +1,12 @@
 public class Piocheur3 extends Carte {
 	
-	public Piocheur3(NumeroCarte num, TypCarte typ) {
+	public Piocheur3(NumerosCarte num, TypCarte typ) {
 		super(num, typ, 15);
 		// TODO Auto-generated constructor stub
 	}
 
-	public void appliquerPouvoir(Joueur joueur, Pioche pioche) {
-		pioche.distribuerCarte(3, joueur);   
+	public void appliquerPouvoir(Manche manche) {
+		manche.pioche.distribuerCarte(3, manche.joueurSuivant(this)); 
 	 }
 	
 	public static void main(String[] args ) {
