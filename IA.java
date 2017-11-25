1da104402b945fc9ab1d58b2a1fea2bb72cc67b7
freddysob <32365266+freddysob@users.noreply.github.com>
2017-11-26 00:21:38 +0100

    Update IA.java

--- IA.java
+++ IA.java
@@ -3,7 +3,7 @@ import java.util.List;
 //import com.modeliosoft.modelio.javadesigner.annotations.objid;
 
 //@objid ("ce110b50-f8ea-48ea-8017-a2104bc861e8")
-public class IA {
+public class IA extends Joueur{
     //@objid ("9dc00c79-ce71-45b1-84b2-6186938b3244")
     private SituationIA situation;
 
@@ -11,8 +11,8 @@ public class IA {
     public Joueur joueur;
     
   //@objid ("b95b7218-d770-490e-a1de-e48f4de5c815")
-    public IA() {
-    	
+    public IA(String name, int num, Hand hand, Manche manche) {
+    	super("IA "+ num +"",num, hand, manche);
     }
 
     //@objid ("35398e5b-9731-402b-829e-bb09bce54f58")
@@ -32,13 +32,14 @@ public class IA {
     public void jouer() {
     	int poids = -100;
     	Carte carteajouer=null;
-    	for(int i=0; i<this.joueur.hand.carte.size();i++) {
-    		if(poids<this.joueur.hand.carte.get(i).getPoids()) {
-    			poids = this.joueur.hand.carte.get(i).getPoids();
-    			carteajouer=this.joueur.hand.carte.get(i);
+    	int i=0;
+    	for(i=0; i<this.hand.carte.size();i++) {
+    		if(poids<this.hand.carte.get(i).getPoids()) {
+    			poids = this.hand.carte.get(i).getPoids();
+    			carteajouer=this.hand.carte.get(i);
     		}
     	}
-    	this.joueur.jouerCarte(carteajouer);
+    	this.jouerCarte(carteajouer);
     }
 
     //@objid ("5db48821-e1bb-4323-8520-b4a682114998")
