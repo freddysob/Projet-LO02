811e349ff48719ac7001b1b6c149580f722be755
freddysob <32365266+freddysob@users.noreply.github.com>
2017-11-25 18:39:34 +0100

    Update Tatamis.java

--- Tatamis.java
+++ Tatamis.java
@@ -1,12 +1,16 @@
 import java.util.ArrayList;
 import java.util.List;
+//import com.modeliosoft.modelio.javadesigner.annotations.objid;
 
 
+//@objid ("39ddc4b7-4a06-46cc-9241-1d13d85ca06c")
 public class Tatamis {
-  
+    //@objid ("e2025e3e-4ae1-42d6-8b82-6d62d090d799")
     public List<Carte> carte = new ArrayList<Carte> ();
     
-   
+    public TypCarte type;
+    
+    //@objid ("4df1b0b0-44af-47ce-898f-fa61b1f58332")
     public Tatamis() {
     	this.carte = new ArrayList<Carte> ();
     }
@@ -17,27 +21,34 @@ public class Tatamis {
     	return s;
     }
     
-   
+    //@objid ("d61f72cf-3977-4030-a554-44a4d78f1cdb")
     protected void finalize() {
     }
 
-    
-    public boolean[] verifierValiditeCarte(Variante Variante) {
-    	boolean s[]={true,true};
-    	return s;
+    public boolean verifierValiditeCarte(/*Variante Variante*/) {
+    	if(this.carte.get(0).getType()==this.type||(type==TypCarte.JN &&(this.carte.get(0).getType()==TypCarte.Pique || this.carte.get(0).getType()==TypCarte.Trefle))||(type==TypCarte.JR &&(this.carte.get(0).getType()==TypCarte.Coeur || this.carte.get(0).getType()==TypCarte.Carreau))||this.carte.get(0).getNumero()==this.carte.get(1).getNumero()||this.carte.get(0) instanceof Commande)
+    	{
+    		return(true);
+    	}else {
+    		return(false);
+    	}
     }
 
-
+    //@objid ("cf7e2a4e-9aaf-4915-980e-6a6906efd6eb")
     public void setCarte(List<Carte> value) {
-        
+        // Automatically generated method. Please delete this comment before entering specific code.
         this.carte = value;
     }
 
-   
+    //@objid ("fea85c9a-1dac-477b-a672-fa355ba74452")
     public List<Carte> getCarte() {
-       
+        // Automatically generated method. Please delete this comment before entering specific code.
         return this.carte;
     }
+    
+    public void ajouterCarte(Carte carte) {
+    	this.carte.add(0, carte);
+    }   
     
 	public static void main(String[] args ) {
     	
