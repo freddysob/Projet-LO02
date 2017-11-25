e788c9dc116c9afdc32aa7f5cd3876c15a348b1c
AutokratorBobba <33391271+AutokratorBobba@users.noreply.github.com>
2017-11-25 20:32:33 +0100

    modif IA

--- IA.java
+++ IA.java
@@ -1,25 +1,45 @@
 package PackageLO02;
 
-import java.util.List;
+import java.util.ArrayList;
 
 public class IA {
     private SituationIA situation;
 
     public Joueur joueur;
 
-    public List<Integer> prioriserCartes() {
-    }
+ /*   public List<Integer> prioriserCartes() {
+    } */
 
     public void jouer() {
     	int poids = -100;
     	Carte carteajouer=null;
+    	ArrayList<Carte> carteJouable = new ArrayList<Carte>();
     	for(int i=0; i<this.joueur.hand.carte.size();i++) {
-    		if(poids<this.joueur.hand.carte.get(i).getPoids()) {
-    			poids = this.joueur.hand.carte.get(i).getPoids();
-    			carteajouer=this.joueur.hand.carte.get(i);
+    		if(this.joueur.manche.pioche.carte.get(0).getType()==this.joueur.hand.carte.get(i).getType()||this.joueur.manche.pioche.carte.get(0).getNumero()==this.joueur.hand.carte.get(i).getNumero()||this.joueur.hand.carte.get(i) instanceof Commande) {
+    			carteJouable.add(this.joueur.hand.carte.get(i));
     		}
     	}
-    	this.joueur.jouerCarte(carteajouer);
+    	if(!carteJouable.isEmpty()) {
+        	for(int i=0; i<carteJouable.size();i++) {
+        		if(poids<carteJouable.get(i).getPoids()) {
+        			poids = carteJouable.get(i).getPoids();
+        			carteajouer=carteJouable.get(i);
+        		}
+        	this.joueur.jouerCarte(carteajouer);
+        	}
+    	}else{
+    		if(Math.random()<0.2) {
+            	for(int i=0; i<carteJouable.size();i++) {
+            		if(poids<carteJouable.get(i).getPoids()) {
+            			poids = carteJouable.get(i).getPoids();
+            			carteajouer=carteJouable.get(i);
+            		}
+            	this.joueur.jouerCarte(carteajouer);
+            	}
+    		}else {
+    			this.joueur.manche.pioche.distribuerCarte(1, this.joueur);
+    		}
+    	}
     }
 
     SituationIA getSituation() {
