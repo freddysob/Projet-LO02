e1450c51e2985412b1bcb8b201c1cc6fe4579767
freddysob <32365266+freddysob@users.noreply.github.com>
2017-11-25 18:19:58 +0100

    Update IA.java

--- IA.java
+++ IA.java
@@ -1,37 +1,58 @@
+import java.util.ArrayList;
+import java.util.List;
+//import com.modeliosoft.modelio.javadesigner.annotations.objid;
+
+//@objid ("ce110b50-f8ea-48ea-8017-a2104bc861e8")
 public class IA {
+    //@objid ("9dc00c79-ce71-45b1-84b2-6186938b3244")
     private SituationIA situation;
 
+    //@objid ("eb467e72-172b-4013-b7f4-c5022868e87a")
     public Joueur joueur;
+    
+  //@objid ("b95b7218-d770-490e-a1de-e48f4de5c815")
+    public IA() {
+    	
+    }
 
-    public int[] prioriserCartes() {
-    	int[] s={0,0,0};
+    //@objid ("35398e5b-9731-402b-829e-bb09bce54f58")
+    protected void finalize() {
+    }
+    
+	public static void main(String[] args ) {
+    	
+    }
+
+    //@objid ("3ce5ca07-26e2-49b1-86f3-210856435573")
+    public List<Integer> prioriserCartes() {
+    	List<Integer> s = new ArrayList<Integer> ();
     	return s;
     }
 
     public void jouer() {
+    	int poids = -100;
+    	Carte carteajouer=null;
+    	for(int i=0; i<this.joueur.hand.carte.size();i++) {
+    		if(poids<this.joueur.hand.carte.get(i).getPoids()) {
+    			poids = this.joueur.hand.carte.get(i).getPoids();
+    			carteajouer=this.joueur.hand.carte.get(i);
+    		}
+    	}
+    	this.joueur.jouerCarte(carteajouer);
     }
 
+    //@objid ("5db48821-e1bb-4323-8520-b4a682114998")
     SituationIA getSituation() {
         // Automatically generated method. Please delete this comment before entering specific code.
         return this.situation;
     }
 
+    //@objid ("5a3817a6-fdb1-454b-9824-e4122dbb2f86")
     void setSituation(SituationIA value) {
         // Automatically generated method. Please delete this comment before entering specific code.
         this.situation = value;
     }
 
-    public IA() {
-    	
-    }
-
-	public IA(Joueur joueur) {
-		this.situation = SituationIA.Normale;
-		this.joueur = joueur;
-	}
-
-	public static void main(String[] args ) {
-    	
-    }
+    
 
 }
