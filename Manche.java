144dcbf52efe17f2d30e6c1c56be5b49cb209a66
freddysob <32365266+freddysob@users.noreply.github.com>
2017-11-25 20:24:05 +0100

    Update Manche.java

--- Manche.java
+++ Manche.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.ListIterator;
 import java.util.List;
 //import com.modeliosoft.modelio.javadesigner.annotations.objid;
 
@@ -56,6 +57,14 @@ public class Manche {
 		pioche.distribuerCarte(nombre, joueur); 
     }
 
+    public void initialiserJoueur() {
+		for(int i=0; i<this.joueur.size(); i++) {
+			this.joueur.get(i).setEtatActif(false);
+			this.joueur.get(i).setaFini(false);
+		}
+		this.joueur.get((int)(Math.random()*(this.joueur.size()))).setEtatActif(true);
+    }
+    
     //@objid ("13f3387a-aee6-495e-bb3e-bb47d80b2711")
     Partie getPartie() {
         // Automatically generated method. Please delete this comment before entering specific code.
@@ -116,67 +125,76 @@ public class Manche {
         this.variante = value;
     }
     
-    public void joueurSuivant(Carte cartePose) {
+    public Joueur joueurSuivant(Carte cartePose) {
+    	ListIterator<Joueur> li= joueur.listIterator();
+    	Joueur joue = null;
     	if(cartePose instanceof Rejoueur) {
-    		//return;
+    		while(li.hasNext()) {
+    			joue = li.next();
+    			if(joue.isEtatActif()) {
+    				return(joue);
+    			}
+    		}
+    	}
+    	while(li.hasNext()) {
+    		if(li.next().isaFini()) {
+    			li.remove();
+    		}
+    	}
+    	while(li.hasPrevious()) {
+    		li.previous();
     	}
     	if(this.sensJeuPos) {
-	    	if(cartePose instanceof StopperSuivant) {
-	    		for(int i=0; i<this.joueur.size();i++) {
-	        		if(this.joueur.get(i).isEtatActif()) {
-	        			this.joueur.get(i).setEtatActif(false);
-	        			if(this.joueur.get(i+1)!= null) {
-	        				if(this.joueur.get(i+2) != null) {
-	        					this.joueur.get(i+2).setEtatActif(true);
-	        				}
-	        				this.joueur.get(1).setEtatActif(true);
-	        			}else {
-	        				this.joueur.get(0).setEtatActif(true);
-	        			}
-	        		}
-	    		}
-	    	}else {
-	    		for(int i=0; i<this.joueur.size();i++) {
-	        		if(this.joueur.get(i).isEtatActif()) {
-	        			this.joueur.get(i).setEtatActif(false);
-	        			if(this.joueur.get(i+1)!= null) {
-	        				this.joueur.get(i+1).setEtatActif(true);
-	        			}else {
-	        				this.joueur.get(0).setEtatActif(true);
-	        			}
-	        		}
-	    		}
-	    	}
+    		while(!li.next().isEtatActif()) {
+    			li.next();
+    		}
+    		if(cartePose instanceof StopperSuivant) {
+    			if(!li.hasNext()) {
+    		    	while(li.hasPrevious()) {
+    		    		li.previous();
+    		    	}
+    			}else {
+    				li.next();
+    			}
+    		}
+    		if(!li.hasNext()) {
+    		    while(li.hasPrevious()) {
+    		    	li.previous();
+    		    }
+    		    li.next();
+    		    joue = li.previous();
+    		}else {
+    			joue = li.next();
+    		}    		
     	}else {
-	    	if(cartePose instanceof StopperSuivant) {
-	    		for(int i=0; i<this.joueur.size();i++) {
-	        		if(this.joueur.get(i).isEtatActif()) {
-	        			this.joueur.get(i).setEtatActif(false);
-	        			if(this.joueur.get(i-1)!= null) {
-	        				if(this.joueur.get(i-2) != null) {
-	        					this.joueur.get(i-2).setEtatActif(true);
-	        				}
-	        				this.joueur.get(this.joueur.size()-1).setEtatActif(true);
-	        			}else {
-	        				this.joueur.get(this.joueur.size()).setEtatActif(true);
-	        			}
-	        		}
-	    		}
-	    	}else {
-	    		for(int i=0; i<this.joueur.size();i++) {
-	        		if(this.joueur.get(i).isEtatActif()) {
-	        			this.joueur.get(i).setEtatActif(false);
-	        			if(this.joueur.get(i-1)!= null) {
-	        				this.joueur.get(i-1).setEtatActif(true);
-	        			}else {
-	        				this.joueur.get(this.joueur.size()).setEtatActif(true);
-	        			}
-	        		}
-	    		}
-	    	}
+       		while(!li.next().isEtatActif()) {
+    			li.next();
+    		}
+    		if(cartePose instanceof StopperSuivant) {
+    			if(!li.hasPrevious()) {
+    		    	while(li.hasNext()) {
+    		    		li.next();
+    		    	}
+    			}else {
+    				li.previous();
+    			}
+    		}
+    		if(!li.hasPrevious()) {
+    		    while(li.hasNext()) {
+    		    	li.next();
+    		    }
+    		    li.previous();
+    		    joue = li.next();
+    		}else {
+    			joue = li.previous();
+    		}    		
+
+    	}for(int i=0; i<joueur.size(); i++) {
+    		joueur.get(i).setEtatActif(false);
     	}
-	    	
-	    }
+    	joue.setEtatActif(true);
+    	return(joue);
+	}
     
 	public static void main(String[] args ) {
     	
