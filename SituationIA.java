42e81ecc667670b8e796180ef4d82513f2cb9e19
AutokratorBobba <33391271+AutokratorBobba@users.noreply.github.com>
2017-12-28 16:52:04 +0100

    Delete SituationIA.java

--- SituationIA.java
+++
@@ -1,8 +0,0 @@
-//import com.modeliosoft.modelio.javadesigner.annotations.objid;
-
-//@objid ("3f141fc2-2d20-4e7b-9a5b-67bd4847a3d3")
-public enum SituationIA {
-    Normale,
-    _1JoueurAvance,
-    _2JoueursAvances;
-}
